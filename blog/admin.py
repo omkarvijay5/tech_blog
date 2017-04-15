e66cd9454870714615ec4dd71f820880be0c67e3
omkarvijay5 <omkarvijay5@gmail.com>
2017-04-15 13:00:19 +0530

    added admin page for the Article

--- blog/admin.py
+++ blog/admin.py
@@ -5,9 +5,14 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # third party imports
-
+from blog.models import Category, Article
 # Register your models here.
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-	pass
+    pass
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    pass
