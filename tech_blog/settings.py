ca0505f6d81b499b8bd0b7c56e0410eaf11a6fef
omkarvijay5 <omkarvijay5@gmail.com>
2017-04-15 22:40:43 +0530

    static root, static url, media root and media url setup

--- tech_blog/settings.py
+++ tech_blog/settings.py
@@ -118,5 +118,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = BASE_DIR + "/staticfiles"
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_ROOT = BASE_DIR + "/mediafiles/"
+MEDIA_URL = '/media/'
