ca7076c60c3aa20151572688122796dff2cd1d34
omkarvijay5 <omkarvijay5@gmail.com>
2017-04-15 12:40:20 +0530

    category admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# django imports
 from django.contrib import admin
 
+# third party imports
+
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	pass
