6b396eaf8a133aced46052fde7e8d643e3359785
omkarvijay5 <omkarvijay5@gmail.com>
2017-04-15 12:35:13 +0530

    define Article model to have fields title, author, pub_date, category, hero image, optional image and text

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# python imports
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
@@ -12,3 +15,14 @@ class Category(models.Model):
 
 	def __unicode__(self):
 		return self.name
+
+
+class Article(models.Model):
+	title = models.CharField(max_length=255)
+	author = models.ForeignKey('settings.AUTH_USER_MODEL')
+	pub_date = models.DateTimeField(default=datetime.now, help_text='Publication date')
+	category = models.ForeignKey('blog.Category')
+	hero_image = models.ImageField(upload_to='blog_images/')
+	optional_image = models.ImageField(null=True, blank=True)
+	text = models.TextField()
+
