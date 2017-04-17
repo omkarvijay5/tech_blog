3ef7e40fd196e9433a775524663534baac6ef24c
omkarvijay5 <omkarvijay5@gmail.com>
2017-04-17 11:12:22 +0530

    convert pub_date to Weekday, month, date, year format and make author to return full name or username

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,3 +1,6 @@
+# python imports
+from datetime import datetime
+
 # third party imports
 from rest_framework import serializers
 
@@ -11,3 +14,15 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
+
+    def to_representation(self, article):
+        rep = super(ArticleSerializer, self).to_representation(article)
+
+        # convert pub datetime to proper weekday, month date year format
+        pub_date = rep['pub_date']
+        rep['pub_date'] = datetime.strptime(pub_date, "%Y-%m-%dT%H:%M:%SZ").strftime("%A, %B %d %Y")
+
+        # get the authors first name and last name or username
+        author = article.author
+        rep['author'] = author.get_full_name() or author.username
+        return rep
