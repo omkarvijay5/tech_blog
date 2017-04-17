4f197d2f168af0f57c917b9775045d396af0535c
omkarvijay5 <omkarvijay5@gmail.com>
2017-04-17 09:26:37 +0530

    index page with angular js script tags and settings

--- tech_blog/urls.py
+++ tech_blog/urls.py
@@ -18,11 +18,11 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
-from blog.views import articles
+from blog.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/blog/', include('blog.urls')),
 
-    url(r'^$', articles, name='articles'),
+    url(r'^$', index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
