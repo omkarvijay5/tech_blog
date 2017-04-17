ad9cd3469d1f23fee185980a7333397f98f15b40
omkarvijay5 <omkarvijay5@gmail.com>
2017-04-17 11:14:00 +0530

    logic to get the random article in the controller

--- static/js/controllers/ArticleController.js
+++ static/js/controllers/ArticleController.js
@@ -3,8 +3,19 @@ function ArticleListController($scope, $resource, ArticleListService) {
 
     ctrl.articles = ArticleListService.get_articles().
     then(function success(response) {
-        ctrl.articles = response.data;
-        console.log(ctrl.articles)
+        var articles = response.data;
+        if (articles.length > 0) {
+            var min = 0
+            var max = articles.length - 1
+            random_index = Math.round(Math.random() * (max - min) + min, 1);
+            ctrl.random_article = articles[random_index]
+            articles.splice(random_index, 1)
+            ctrl.articles = articles
+            console.log(random_index)
+        } else {
+            ctrl.articles = articles
+            ctrl.random_article = null
+        }
     });
 }
 
