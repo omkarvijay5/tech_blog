fbfe5c1432648c09c51fb05ee8f5cd85d44bf602
omkarvijay5 <omkarvijay5@gmail.com>
2017-04-18 14:28:23 +0530

    render media and static in urls

--- tech_blog/urls.py
+++ tech_blog/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     url(r'^api/v1/blog/', include('blog.urls')),
 
     url(r'^$', index, name='index'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
