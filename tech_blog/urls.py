20f28465e7b79a97e7c8af3e908e3c0096a5f599
omkarvijay5 <omkarvijay5@gmail.com>
2017-04-18 13:53:41 +0530

    serve static files in urls

--- tech_blog/urls.py
+++ tech_blog/urls.py
@@ -26,3 +26,5 @@ urlpatterns = [
 
     url(r'^$', index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
